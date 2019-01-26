27cfcacfeaca548fec05cc03148071e2c315ca24
Hanan Kavitz <hananke0@gmail.com>
2019-01-26 20:33:38 +0200

    add firebase-funcions

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var app = require('express')();
 var port = 8080;
 
+const functions = require("firebase-functions");
+
 app.use('/', function(req,res) {
     res.status(200).send('Hello world');
 });
 
-app.listen(port, function(){
-    console.log(`Server is running on ${port}`)
-})
+
+const api = functions.https.onRequest(app);
+// app.listen(port, function(){
+//     console.log(`Server is running on ${port}`)
+// })
 
 // exports.helloWorld = function(req,res) {
 //     res.status(200).send('Hello world');
 // }
+module.exports = {
+    app
+}

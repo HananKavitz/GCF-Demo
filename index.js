ba6a90ea51045bb84c8541102c9ac416df02b417
Hanan Kavitz <hananke0@gmail.com>
2019-01-27 08:04:32 +0200

    app

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var app = require('express')();
 var port = 8080;
 
-const functions = require("firebase-functions");
+// const functions = require("firebase-functions");
 
 app.use('/', function(req,res) {
     res.status(200).send('Hello world');
 });
 
 
-const api = functions.https.onRequest(app);
+// const api = functions.https.onRequest(app);
 // app.listen(port, function(){
 //     console.log(`Server is running on ${port}`)
 // })
@@ -16,6 +16,7 @@ const api = functions.https.onRequest(app);
 // exports.helloWorld = function(req,res) {
 //     res.status(200).send('Hello world');
 // }
+
 module.exports = {
     app
 }
